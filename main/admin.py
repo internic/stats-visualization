a363c011ed028b8998dfd8e8b712526faf5be109
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2024-01-04 01:05:25 +0600

    Registering all the models to admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Household, Individual, Education, ChildHealth, ChildHealthDetails, HouseholdProperty, HouseholdAsset, HouseholdFinance, HouseholdFoodConsumption, HouseholdNonFoodExpenditure, HouseholdIncome, HouseholdTransfer, HouseholdShock, HouseholdClimateChangeResponse
 
-# Register your models here.
+admin.site.register(Household)
+admin.site.register(Individual)
+admin.site.register(Education)
+admin.site.register(ChildHealth)
+admin.site.register(ChildHealthDetails)
+admin.site.register(HouseholdProperty)
+admin.site.register(HouseholdAsset)
+admin.site.register(HouseholdFinance)
+admin.site.register(HouseholdFoodConsumption)
+admin.site.register(HouseholdNonFoodExpenditure)
+admin.site.register(HouseholdIncome)
+admin.site.register(HouseholdTransfer)
+admin.site.register(HouseholdShock)
+admin.site.register(HouseholdClimateChangeResponse)
+
